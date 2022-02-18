8a3be7ee50783b6fead8c66a0f2ccfeb582e4c0a
ingong <insong1216@gmail.com>
2022-02-18 20:13:58 +0900

    design: global css, theme, common 수정

---
+++ styles/common.ts
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const Center = `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SpaceBetween = `
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const SpaceAround = `
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const SpaceEvenly = `
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
