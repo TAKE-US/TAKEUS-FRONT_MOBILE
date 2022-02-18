c255f7f2dda5a67ac0f8e5792d17945c33a2b25e
ingong <insong1216@gmail.com>
2022-02-18 11:56:48 +0900

    init: 기본 페이지 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next';
 import { ReactElement } from 'react';
+import styled from '@emotion/styled';
+
+const Header = styled.div`
+  background-color: ${(props) => props.theme.color.primary}
+`
 
 const Home: NextPage = (): ReactElement => {
-  return <div>Home</div>;
+  return <Header>Home</Header>
 };
 
 export default Home;
