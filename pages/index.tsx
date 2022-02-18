a7dc217a759d5890135ddf9cf41a60b93f77e188
ingong <insong1216@gmail.com>
2022-02-18 20:14:12 +0900

    feat: 헤더 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,32 @@ import type { NextPage } from 'next';
 import { ReactElement } from 'react';
 import styled from '@emotion/styled';
 
-const Header = styled.div`
-  background-color: ${(props) => props.theme.color.primary}
+import HamburgerBtn from '@assets/hamburgerbtn.svg';
+import TakeusLogo from '@assets/takeuslogo.svg'
+
+import { SpaceBetween } from '@styles/common';
+
+
+const Header = styled.header`
+  ${SpaceBetween}
+  height: 5rem;
+  margin: 4rem 2rem 0 2rem;
+`
+
+const LoginButton = styled.button`
+  border: 1px solid;
+  border-radius: 4px;
+  font: ${({ theme }) => theme.font.smallheading1_btn};
 `
 
 const Home: NextPage = (): ReactElement => {
-  return <Header>Home</Header>
+  return (
+      <Header>
+        <HamburgerBtn />
+        <TakeusLogo />
+        <LoginButton>로그인</LoginButton>
+      </Header>
+    )
 };
 
 export default Home;
