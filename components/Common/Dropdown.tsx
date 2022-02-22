ac4b6c76ec8f113784a85b9f0ae42e6902f6646d
ingong <insong1216@gmail.com>
2022-02-22 14:12:09 +0900

    feat: reusable Dropdown 컴포넌트 추가

---
+++ components/Common/Dropdown.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+import DropdownArrow from '@assets/DropdownArrow.svg';
+import { SpaceBetween } from '@styles/common';
+import { ReactElement } from 'react';
+
+const DropdownContainer = styled.div`
+  ${SpaceBetween}
+  font: ${({ theme }) => theme.font.body1_medium};
+  width: 45%;
+  & > p {
+    color: ${({ theme }) => theme.color.gray3};
+  }
+`;
+
+interface DropdownSelectProp<T> {
+  name: string;
+  data?: Array<T>;
+}
+
+interface DataProp {
+  name: string;
+}
+
+const Dropdown = ({ name, data }: DropdownSelectProp<Array<DataProp>>): ReactElement => {
+  return (
+    <DropdownContainer>
+      <p>{name}</p>
+      <DropdownArrow />
+    </DropdownContainer>
+  );
+};
+
+export default Dropdown;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,16 @@ import type { NextPage } from 'next';
 import { ReactElement } from 'react';
 import styled from '@emotion/styled';
 
-import HamburgerBtn from '@assets/hamburgerbtn.svg';
-import TakeusLogo from '@assets/takeuslogo.svg'
-
-import { SpaceBetween } from '@styles/common';
-
-
-const Header = styled.header`
-  ${SpaceBetween}
-  height: 5rem;
-  margin: 4rem 2rem 0 2rem;
-`
-
-const LoginButton = styled.button`
-  border: 1px solid;
-  border-radius: 4px;
-  font: ${({ theme }) => theme.font.smallheading1_btn};
-`
+import Header from '@components/Header';
+import SearchBar from '@components/SearchBar';
 
 const Home: NextPage = (): ReactElement => {
   return (
-      <Header>
-        <HamburgerBtn />
-        <TakeusLogo />
-        <LoginButton>로그인</LoginButton>
-      </Header>
-    )
+    <>
+      <Header />
+      <SearchBar />
+    </>
+  );
 };
 
 export default Home;
