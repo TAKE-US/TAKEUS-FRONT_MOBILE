6397a2a1674cdf5d125939b2a21dc11423a3293a
ingong <insong1216@gmail.com>
2022-02-28 12:56:32 +0900

    feat: 화면크기에 맞는 컴포넌트 분기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,63 @@
-import type { NextPage } from 'next';
-import { ReactElement } from 'react';
 import styled from '@emotion/styled';
 
-import { VerticalAlign } from '@styles/common';
+import { SpaceBetween, VerticalAlign } from '@styles/common';
 import Header from '@components/Header';
 import SearchBar from '@components/SearchBar';
 import DogCardContainer from '@components/Carousel/DogCardContainer';
 import MainPageInform from '@components/MainPageInform';
 
 import { getDogs } from '@service/network';
+import { DogCardListType } from '../types/dog';
+import Loading from '@assets/Loading.gif';
+import Image from 'next/image';
 
 const NextPageContainer = styled.div`
   ${VerticalAlign};
   justify-content: space-between;
+  align-items: center;
+
+  & > .mobile {
+    display: none;
+    @media all and (max-width: 768px) {
+      display: block;
+    }
+  }
+
+  & > .desktop {
+    ${VerticalAlign};
+    align-items: center;
+    margin-top: 15rem;
+    font: ${({ theme }) => theme.font.title1};
+    @media all and (max-width: 768px) {
+      display: none;
+    }
+  }
 `;
 
-const Home: NextPage = (): ReactElement => {
+const Home = ({ data }: { data: DogCardListType }) => {
   return (
     <NextPageContainer>
-      <Header />
-      <SearchBar />
-      <DogCardContainer />
-      <MainPageInform />
+      <div className="mobile">
+        <Header />
+        <SearchBar />
+        <DogCardContainer data={data} />
+        <MainPageInform />
+      </div>
+      <div className="desktop">
+        <p>모바일 버전입니다.</p>
+        <p>화면 크기를 더 줄여주세요 :)</p>
+        <Image src={Loading} width={450} height={450} alt="Loading Image" />
+      </div>
     </NextPageContainer>
   );
 };
 
 export async function getStaticProps() {
-  const dogList = await getDogs();
+  const { data } = await getDogs();
+  const dogListwithCarousel = data?.dogList.slice(0, 10);
   return {
     props: {
-      dogList: [],
+      data: dogListwithCarousel,
     },
     revalidate: 2000,
   };
