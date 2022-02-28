4d131403cfbd32688600e5fdd2b3b10f22535458
ingong <insong1216@gmail.com>
2022-02-28 12:53:43 +0900

    fix: 모달 컴포넌트 및 훅 typescript 에러 수정

--- hooks/useModals.ts
+++ hooks/useModals.ts
@@ -3,17 +3,17 @@ import { ModalsDispatchContext } from '@context/ModalsContext';
 import { ModalsDispatch } from '../types/modal';
 
 export default function useModals() {
-  const { open, close } = useContext(ModalsDispatchContext) as ModalsDispatch
+  const { open, close } = useContext(ModalsDispatchContext) as ModalsDispatch;
 
   const openModal = (Component: React.ReactNode, props?: object) => {
     open(Component, props);
-  }
+  };
   const closeModal = (Component: React.ReactNode) => {
-    close(Component)
-  }
-  
+    close(Component);
+  };
+
   return {
     openModal,
-    closeModal
-  }
+    closeModal,
+  };
 }
