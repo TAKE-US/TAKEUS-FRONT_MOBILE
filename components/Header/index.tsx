ec59e16382d6a926d082620c466d1f6717e35192
ingong <insong1216@gmail.com>
2022-03-02 02:19:51 +0900

    feat: 토큰 유효시간을 기준으로 로그인 상태 관리

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
 
+import { useState, useEffect } from 'react';
 import { modalList } from '@components/Modals';
 import useModals from '@hooks/useModals';
 
@@ -24,18 +25,36 @@ const LoginButton = styled.button`
 const Header = () => {
   const router = useRouter();
   const { openModal } = useModals();
+  const [isLogin, setIsLogin] = useState<boolean | null>(null);
+
   const handleSidebar = () => {
     openModal(modalList.SideBar);
   };
   const handleLoginButton = () => {
     router.push('/login');
   };
+  const handleLogoutButton = () => {
+    localStorage.clear();
+    setIsLogin(null);
+    router.push('/');
+  };
+
+  useEffect(() => {
+    const getIssuedAtTime = () => localStorage.getItem('issuedAt');
+    const getNowTime = () => Math.floor(+new Date() / 1000);
+    const ONE_HOUR = 3600;
+    const isValidToken = getNowTime() - Number(getIssuedAtTime()) < ONE_HOUR;
+
+    setIsLogin(isValidToken);
+  }, []);
 
   return (
     <Container>
       <HamburgerBtn onClick={handleSidebar} />
       <TakeusLogo />
-      <LoginButton onClick={handleLoginButton}>로그인</LoginButton>
+      <LoginButton onClick={!isLogin ? handleLoginButton : handleLogoutButton}>
+        {!isLogin ? '로그인' : '로그아웃'}
+      </LoginButton>
     </Container>
   );
 };
