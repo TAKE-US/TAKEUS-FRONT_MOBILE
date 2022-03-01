29edc27d8b3c72156299afbb23d13af400c4f336
ingong <insong1216@gmail.com>
2022-03-02 01:59:02 +0900

    feat: Google 소셜 로그인 구현

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import styled from '@emotion/styled';
+import { GoogleLogin } from 'react-google-login';
+
+import { postToken } from '@service/network';
+
+import { VerticalAlign, Center, SpaceAround } from '@styles/common';
 import LoginBackGround from '@assets/LoginBackGround.png';
 import LoginTitle from '@assets/LoginTitle.png';
-import { VerticalAlign, Center, SpaceBetween, SpaceAround } from '@styles/common';
-import GoogleButton from '@assets/GoogleButton.svg';
+import GoogleIcon from '@assets/GoogleIcon.svg';
 
 const Container = styled.div`
   width: 100vw;
@@ -18,13 +23,13 @@ const Container = styled.div`
   & > .content {
     z-index: 1;
     position: relative;
-    height: 65%;
-    width: 45%;
+    height: 60%;
+    width: 50%;
     justify-content: space-between;
     ${VerticalAlign}
 
     .title {
-      height: 17%;
+      height: 20%;
       ${VerticalAlign}
       justify-content: space-between;
     }
@@ -54,6 +59,21 @@ const Container = styled.div`
 `;
 
 const Login = () => {
+  const router = useRouter();
+
+  const handleSuccess = async (token: string, social: string) => {
+    const { data, error } = await postToken(token, social);
+    localStorage.setItem('email', data.email);
+    localStorage.setItem('token', data.token);
+    localStorage.setItem('ID', data.id);
+    localStorage.setItem('issuedAt', data.issuedAt);
+
+    router.push('/');
+  };
+
+  //TODO fail 처리할 모달 작성 예정 
+  const handleFailure = () => {console.log('fail')}
+
   return (
     <Container>
       <Image className="background" src={LoginBackGround} alt="LoginBackground" layout="fill" />
@@ -62,11 +82,18 @@ const Login = () => {
           <Image className="takeus" width={147} height={36} src={LoginTitle} alt="TakeUS" />
           <p>SNS 계정으로 손쉽게 가입하고 Takers가 될 수 있어요:)</p>
         </div>
-      </div>
-      <button>
-        <GoogleButton />
-        <p>구글로 시작하기</p>
-      </button>
+        < /div>
+        <GoogleLogin
+          clientId={'490242867421-0i9ikdrk4uql4agn2gr6q57btgvphlga.apps.googleusercontent.com'}
+          render={(renderProps) => (
+            <button onClick={renderProps.onClick}>
+              <GoogleIcon />
+              <p>구글로 시작하기</p>
+            </button>
+          )}
+          onSuccess={(res) => handleSuccess(res.accessToken, 'google')}
+          onFailure={handleFailure}
+        />
     </Container>
   );
 };
