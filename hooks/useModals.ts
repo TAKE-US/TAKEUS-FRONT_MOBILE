2021bb27b181e82d6a4babef7f6099f16befa46b
ingong <insong1216@gmail.com>
2022-03-03 19:10:16 +0900

    feat: 모달 클릭시 scroll 불가능 상태로 변경

--- hooks/useModals.ts
+++ hooks/useModals.ts
@@ -4,12 +4,17 @@ import { ModalsDispatch } from '@Customtypes/modal';
 
 export default function useModals() {
   const { open, close } = useContext(ModalsDispatchContext) as ModalsDispatch;
+  const preventScroll = () => (document.body.style.overflow = 'hidden');
+  const ableScroll = () => (document.body.style.overflow = 'scroll');
 
   const openModal = (Component: React.ReactNode, props?: object) => {
     open(Component, props);
+    preventScroll();
   };
+
   const closeModal = (Component: React.ReactNode) => {
     close(Component);
+    ableScroll();
   };
 
   return {
