5d56b241a2cb5cad71951b7cb5960742d2fdb8d4
ingong <insong1216@gmail.com>
2022-03-04 19:16:05 +0900

    design: SearchBar 스타일 수정

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import Image from 'next/image';
 
 import SearchBarForm from '@components/SearchBarForm';
-import { Container, SearchBarContainer } from './style';
+import { SearchBarContainer, SearchBarContentContainer } from './style';
 import MainBackGroundImage from '@assets/MainBackground.png';
 import { countryAirportType } from '@Customtypes/utils';
 
 const SearchBar = ({ countryAirportList }: { countryAirportList: countryAirportType }) => {
   return (
-    <Container>
-      <Image src={MainBackGroundImage} alt="Main BackGround Image" />
-      <SearchBarContainer>
+    <SearchBarContainer>
+      <Image src={MainBackGroundImage} height={300} width={375} alt="Main BackGround Image" />
+      <SearchBarContentContainer>
         <SearchBarForm countryAirportList={countryAirportList} />
         <p>
           새로운 삶을 찾아 먼 바다를 건너야하는 생명에게 새로운 삶을 <br /> 선물해주는 이들을 우리는
           Takers라고 부릅니다.
         </p>
-      </SearchBarContainer>
-    </Container>
+      </SearchBarContentContainer>
+    </SearchBarContainer>
   );
 };
 
--- components/SearchBar/style.ts
+++ components/SearchBar/style.ts
@@ -1,46 +1,23 @@
 import styled from '@emotion/styled';
-import { SpaceAround, SpaceBetween, VerticalAlign } from '@styles/common';
-
-export const Container = styled.div`
-  position: relative;
-  top: -2rem;
-  z-index: -1;
-`;
+import { VerticalAlign } from '@styles/common';
 
 export const SearchBarContainer = styled.div`
+  position: relative;
+  z-index: 1;
+`;
+
+export const SearchBarContentContainer = styled.div`
   width: 100%;
-  height: 100%;
+  height: 50%;
   ${VerticalAlign};
   align-items: center;
   justify-content: center;
   position: absolute;
-  top: -6rem;
+  top: 0.5rem;
 
   & > p {
     font: ${({ theme }) => theme.font.body2};
     color: ${({ theme }) => theme.color.gray3};
     text-align: center;
-  }
-`;
-
-export const SearchBarFormContainer = styled.div`
-  width: 90%;
-  margin-bottom: 1.5rem;
-  ${SpaceAround}
-
-  & > .dropdown__container {
-    width: 87%;
-    height: 75%;
-    padding: 1rem;
-    border-radius: 0.4rem;
-
-    background-color: ${({ theme }) => theme.color.white};
-    ${SpaceBetween}
-
-    & > .contour {
-      width: 1px;
-      height: 150%;
-      background-color: ${({ theme }) => theme.color.gray1};
-    }
   }
 `;
