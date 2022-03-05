36cbdd55ef7a45b445a8ea63808dcd86b034d9fe
ingong <insong1216@gmail.com>
2022-03-05 20:54:47 +0900

    feat: reducer 를 활용한 강아지페이지 상태관리

--- pages/dogs/index.tsx
+++ pages/dogs/index.tsx
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { useRouter } from 'next/router';
 import Header from '@components/Header';
 import SearchBarForm from '@components/SearchBarForm';
 import { getCountryAndAirport } from '@service/utils';
+import { getDogByAirport } from '@service/dogs';
+import to from 'await-to-js';
 
 import { countryAirportType } from '@Customtypes/utils';
 
 import styled from '@emotion/styled';
 import { VerticalAlign } from '@styles/common';
+import { reducer } from '@utils/Component.reducer';
+import { ERROR_TYPE } from '@service/index';
+import { DogCardListType } from '@Customtypes/dog';
+import { State, Action } from '@utils/Component.reducer';
 
 const DogPageContainer = styled.div`
   height: 100vh;
@@ -33,15 +39,46 @@ const DogPageContainer = styled.div`
 
 const DogsPage = () => {
   const router = useRouter();
-  const [countryAirportList, setList] = useState<countryAirportType>({});
+  const [airportList, setAirportList] = useState<countryAirportType>({});
+  const [dogListState, dispatch] = useReducer<
+    React.Reducer<State<DogCardListType>, Action<DogCardListType>>
+  >(reducer, {
+    _TAG: 'IDLE',
+  });
+
+  const handleDogList = async (airport: string) => {
+    dispatch({
+      _TAG: 'FETCH',
+    });
+
+    const [error, response] = await to(getDogByAirport(airport));
+
+    if (error) {
+      return dispatch({
+        _TAG: 'FAILED',
+        error,
+      });
+    }
+    if (response?.error !== ERROR_TYPE.NOT_ERROR) {
+      return dispatch({
+        _TAG: 'FAILED',
+        error: response?.error,
+      });
+    }
+    if (response?.error === ERROR_TYPE.NOT_ERROR && response.dogList) {
+      return dispatch({ _TAG: 'SUCCESS', message: response.dogList });
+    }
+  };
 
   useEffect(() => {
     const handleList = async () => {
       const { countryAirportList } = await getCountryAndAirport();
-      setList(countryAirportList);
+      setAirportList(countryAirportList);
     };
+
     handleList();
-  }, []);
+    handleDogList(router.query.airport as string);
+  }, [router.query.airport]);
 
   // 처음 진입 : 검색 결과, 공항 리스트,
   // 다시 클릭 시 : 검색 결과만 변경할 수 있도록
@@ -51,9 +88,9 @@ const DogsPage = () => {
   return (
     <DogPageContainer>
       <div className="header__wrapper">
-        <Header />
+        <Header onClick={handleDogList} />
         <p className="content">도움을 기다리는 입양견들을 만나보세요.</p>
-        <SearchBarForm countryAirportList={countryAirportList} />
+        <SearchBarForm countryAirportList={airportList} />
       </div>
       This is the Dog Page
     </DogPageContainer>
