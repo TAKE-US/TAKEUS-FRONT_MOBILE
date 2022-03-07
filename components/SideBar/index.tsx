fdc5687e4f1bbebe5a8f18b20b955c3bbb71d972
ingong <insong1216@gmail.com>
2022-03-07 15:19:33 +0900

    refactor: SideBar 스타일 파일 분리

--- components/SideBar/index.tsx
+++ components/SideBar/index.tsx
@@ -1,54 +1,11 @@
 import React, { ReactElement } from 'react';
-import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
-
 import Overlay from '@components/Common/Overlay';
 import { modalList } from '@components/Modals';
+
 import useModals from '@hooks/useModals';
-
-import { VerticalAlign } from '@styles/common';
 import DeleteIcon from '@assets/DeleteIcon.svg';
-
-const SideBarContainer = styled.aside`
-  ${VerticalAlign}
-  height: 100%;
-  width: 83%;
-  padding-left: 3rem;
-  padding-top: 3rem;
-
-  top: 0;
-  position: absolute;
-  align-items: flex-start;
-  z-index: 3;
-  font: ${({ theme }) => theme.font.title2_btn};
-  background-color: ${({ theme }) => theme.color.white};
-
-  & > .XBtn {
-    width: 90%;
-    height: 8%;
-    display: flex;
-    justify-content: flex-end;
-  }
-
-  & > span {
-    height: 10%;
-  }
-
-  & > ul {
-    ${VerticalAlign}
-    height: 25%;
-    justify-content: space-around;
-  }
-`;
-
-const TotalContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  z-index: 4;
-`;
+import { TotalContainer, SideBarContainer } from './style';
 
 const SideBar = (): ReactElement => {
   const router = useRouter();
